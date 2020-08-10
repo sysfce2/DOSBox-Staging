a3371f4f7548d9808ae69767ceb5cad6870d4019
kcgen <1557255+kcgen@users.noreply.github.com>
2020-08-09 18:47:40 -0700

    [WIP] Minimized fixed cycle scheduler

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -222,23 +222,41 @@ static Bitu Normal_Loop()
 
 void increaseticks_fixed()
 {
-	auto ticksNew = GetTicks();
+	const auto ticksNew = GetTicks();
 	if (ticksNew <= ticksLast) {
-		ticksAdded = 0;
 
 		while (pic_balance < 1)
 			std::this_thread::sleep_for(usT(50));
 		std::lock_guard<std::mutex> guard(pic_balance_mutex);
-		pic_balance--;
-
-		auto timeslept = GetTicks() - ticksNew;
-		return; //0
+		pic_balance = 0;
+		return;
 	}
 
-	ticksRemain = ticksNew-ticksLast;
+	constexpr long int max_ticks_remaing = 20;
+	ticksRemain = std::min(max_ticks_remaing, ticksNew - ticksLast);
 	ticksLast = ticksNew;
+	/* 	if (ticksRemain > 20)
+	                ticksRemain = 20; */
 }
-//For trying other delays
+
+/* uint32_t t_remain = 0;
+uint32_t t_last = 0;
+
+void increaseticks_fixed()
+{
+        const uint32_t t_new = GetTicks();
+        if (t_new <= t_last) {
+                while (pic_balance < 1)
+                        std::this_thread::sleep_for(usT(50));
+                std::lock_guard<std::mutex> guard(pic_balance_mutex);
+                pic_balance = 0;
+                return;
+        }
+        t_remain = std::min(20u, t_new - t_last);
+        t_last = t_new;
+} */
+
+// For trying other delays
 #define wrap_delay(a) SDL_Delay(a)
 
 void increaseticks() { //Make it return ticksRemain and set it in the function above to remove the global variable.
