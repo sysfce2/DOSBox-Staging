efb4889980fcbb8f95ac8c351dbf56e851b79ee6
shermp <14854761+shermp@users.noreply.github.com>
2022-01-30 14:42:43 +1300

    Switch to simple "cs-script" based wixsharp project

---
+++ contrib/windows/installer/Installer.cs
@@ -0,0 +1,64 @@
+//css_dir .\WixSharp;
+//css_ref System.Core.dll;
+//css_ref System.Windows.Forms.dll;
+//css_ref Wix_bin\SDK\Microsoft.Deployment.WindowsInstaller.dll;
+//css_ref WixSharp.dll;
+//css_ref WixSharp.UI.dll;
+
+using System;
+
+using WixSharp;
+using WixSharp.Forms;
+
+class Script 
+{
+    static public void Main(string[] args)
+    {
+        var pkgPath = Environment.GetEnvironmentVariable("DOSBOX_PKG_PATH");
+        if (pkgPath.IsEmpty()) { Console.WriteLine("DOSBOX_PKG_PATH must be set!"); }
+
+        var dosboxVers = Environment.GetEnvironmentVariable("DOSBOX_VERSION");
+        if (dosboxVers.IsEmpty()) { Console.WriteLine("DOSBOX_VERSION must be set!"); }
+
+        var outDir = Environment.GetEnvironmentVariable("DOSBOX_INSTALL_OUTDIR");
+        var outFile = Environment.GetEnvironmentVariable("DOSBOX_INSTALL_OUTFILE");
+
+        Console.WriteLine(string.Format("DOSBOX_PKG_PATH is '{0}'", pkgPath));
+        Console.WriteLine(string.Format("DOSBOX_VERSION is '{0}'", dosboxVers));
+
+        var project = new ManagedProject("Dosbox Staging",
+                          new Dir(@"%ProgramFiles%\Dosbox-Staging\Dosbox Staging",
+                              new Files(string.Format(@"{0}\*.*", pkgPath))));
+ 
+        project.GUID = new Guid("18910652-7759-47c0-aaae-684ae15f18f1");
+
+        project.LicenceFile = "gpl-2.0.rtf";
+
+        project.Version = new Version(dosboxVers);
+        project.Platform = Platform.x64;
+
+        // Allow downgrades
+        project.MajorUpgrade = new MajorUpgrade();
+        project.MajorUpgrade.AllowDowngrades = true;
+
+        project.ManagedUI = new ManagedUI();
+        project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
+                                        .Add(Dialogs.Licence)
+                                        .Add(Dialogs.SetupType)
+                                        .Add(Dialogs.Features)
+                                        .Add(Dialogs.InstallDir)
+                                        .Add(Dialogs.Progress)
+                                        .Add(Dialogs.Exit);
+        
+        project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
+                                    .Add(Dialogs.Features)
+                                    .Add(Dialogs.Progress)
+                                    .Add(Dialogs.Exit);
+
+        if (outDir.IsNotEmpty()) { project.OutDir = outDir; }
+        if (outFile.IsNotEmpty()) { project.OutFileName = outFile; }
+
+        Compiler.BuildMsi(project);
+
+    }
+} 
